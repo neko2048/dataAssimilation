bb018d08b2bec0d64be75ba981afb3d9d94006cb
UC <uc@UCdeiMac.local>
2022-01-03 15:22:26 +0800

    add incre3DVar

--- test.py
+++ test.py
@@ -1,74 +1,100 @@
 import numpy as np
 from scipy.integrate import ode
-from matplotlib.pyplot import *
 import copy
+from scipy.optimize import minimize
+
 from initValueGenerate import Lorenz96
 from parameterControl import *
-from scipy.optimize import minimize
 from dataRecorder import RecordCollector
 
-class threeDVar:
+class increThreeDVar:
     def __init__(self, xInitAnalysis):
         self.xInitAnalysis = xInitAnalysis
         self.obsOperator = np.identity(Ngrid)
 
-    def costFunction(self, analysisState, backgroundState, observationState, backgroundEC, observationEC):
+    def costFunction(self, analysisIncrement, innovation, backgroundEC, observationEC):
         H = self.obsOperator
-        backgroundCost = (backgroundState - analysisState).transpose() @ np.linalg.inv(backgroundEC) @ (backgroundState - analysisState)
+        backgroundCost = (analysisIncrement).transpose() @ np.linalg.inv(backgroundEC) @ (analysisIncrement)
 
-        innovation = observationState - H @ analysisState
-        observationCost = (innovation).transpose() @ np.linalg.inv(observationEC) @ (innovation)
+        observationCost = (H @ analysisIncrement - innovation).transpose() @ np.linalg.inv(observationEC) @ (H @ analysisIncrement - innovation)
         return 0.5 * (backgroundCost + observationCost)
 
-    def gradientOfCostFunction(self, analysisState, backgroundState, observationState, backgroundEC, observationEC):
+    def gradientOfCostFunction(self, analysisIncrement, innovation, backgroundEC, observationEC):
         H = self.obsOperator
-        gradientBackgroundCost = np.linalg.inv(backgroundEC) @ (analysisState - backgroundState)
+        gradientBackgroundCost = np.linalg.inv(backgroundEC) @ (analysisIncrement)
 
-        innovation = observationState - H @ analysisState
-        gradientObservationCost = H.transpose() @ np.linalg.inv(observationEC) @ (innovation)
-        return gradientBackgroundCost - gradientObservationCost
+        gradientObservationCost = H.transpose() @ np.linalg.inv(observationEC) @ (H @ analysisIncrement - innovation)
+        return gradientBackgroundCost + gradientObservationCost
+
+    # forecast
+    def getForecastState(self, analysisState, nowT):
+        lorenz = Lorenz96(initValue=analysisState)
+        lorenz.solver = lorenz.getODESolver(initTime=nowT)
+        backgroundState = lorenz.solveODE(endTime=nowT+dT)
+        return backgroundState
 
     # analyzing
-    def getAnalysisState(self, backgroundState, observationState, backgroundEC, observationEC):
-        analysisState = minimize(self.costFunction, x0=backgroundState, \
-                        args = (backgroundState, observationState, backgroundEC, observationEC), \
-                        method='CG', jac=self.gradientOfCostFunction).x
-        return analysisState
+    def getAnalysisState(self, backgroundState, observationState, backgroundEC, observationEC, NouterLoop=4):
+        guessState = backgroundState
+        for outer in range(NouterLoop):
+            guessState = self.outerLoop(guessState=guessState, \
+                                        observationState = observationState, \
+                                        backgroundEC = backgroundEC, 
+                                        observationEC = observationEC)
+        return guessState
 
-    def getAnalysisEC(self, forecastEC, KalmanGain, inflation=1):
-        pass
+    def outerLoop(self, guessState, observationState, backgroundEC, observationEC, NinnerLoop=5):
+        innovation = observationState - self.obsOperator @ guessState
+        for inner in range(NinnerLoop):
+            analysisIncrement = self.innerLoop(initGuessState = guessState, \
+                                               innovation = innovation, \
+                                               backgroundEC = backgroundEC, \
+                                               observationEC = observationEC)
+        guessState += analysisIncrement
+        return guessState
+
+    def innerLoop(self, initGuessState, innovation, backgroundEC, observationEC):
+        analysisIncrement = minimize(self.costFunction, x0=initGuessState, \
+                            args = (innovation, backgroundEC, observationEC), \
+                            method='CG', jac=self.gradientOfCostFunction).x
+        return analysisIncrement
 
 if __name__ == "__main__":
     # states
     xInitAnalysis = np.loadtxt("initRecord/xAnalysisInit.txt")
     xFullObservation = np.loadtxt("initRecord/xObservation_{}.txt".format(noiseType))
-    print(xFullObservation.shape)
     xTruth = np.loadtxt("initRecord/xTruth.txt")
 
     # covariance 
     analysisEC = np.loadtxt("initRecord/initEC_{}.txt".format(noiseType))
-    observationEC = np.identity(Ngrid) * noiseScale
+    observationEC = np.identity(Ngrid) * (noiseScale ** 2)
 
     # collector
-    dataRecorder = RecordCollector(methodName="threeDVar", noiseType=noiseType)
+    dataRecorder = RecordCollector(methodName="increThreeDVar", noiseType=noiseType)
 
     # initial setup
-    threeDvar = threeDVar(xInitAnalysis)
-    threeDvar.backgroundState = xInitAnalysis # presumed
-    threeDvar.backgroundEC = analysisEC # presumed
-    threeDvar.analysisState = xInitAnalysis
-    threeDvar.analysisEC = analysisEC
-    threeDvar.observationState = xFullObservation[0]
-    threeDvar.observationEC = observationEC
-    print("{NT:02f}: {ERROR:05f}".format(NT=0, ERROR=np.sum((threeDvar.analysisState - xTruth[i+1])**2)))
+    increThreeDvar = increThreeDVar(xInitAnalysis)
+    increThreeDvar.analysisState = xInitAnalysis
+    increThreeDvar.analysisEC = analysisEC
+    increThreeDvar.forecastState = xInitAnalysis # presumed
+    increThreeDvar.forecastEC = analysisEC # presumed
+    increThreeDvar.observationState = xFullObservation[0]
+    increThreeDvar.observationEC = observationEC
+    increThreeDvar.RMSE = np.sqrt(np.mean((increThreeDvar.analysisState - xTruth[0])**2))
+    increThreeDvar.MeanError = np.mean(increThreeDvar.analysisState - xTruth[0])
+    dataRecorder.record(increThreeDvar, tidx=0)
+    print("{:02f}: {:05f}".format(0, increThreeDvar.RMSE))
+    for tidx, nowT in enumerate(timeArray[:-1]):
+        increThreeDvar.observationState = xFullObservation[tidx+1]
 
-    for i, nowT in enumerate(timeArray[:-1]):
-        threeDvar.observationState = xFullObservation[i+1]
-        threeDvar.analysisState = threeDvar.getAnalysisState(backgroundState = threeDvar.backgroundState, \
-                                                             observationState = threeDvar.observationState, \
-                                                             backgroundEC = threeDvar.backgroundEC, \
-                                                             observationEC = threeDvar.observationEC)
-        print("{NT:02f}: {ERROR:05f}".format(NT=nowT+dT, ERROR=np.sqrt(np.mean((threeDvar.analysisState - xTruth[i+1])**2))))
+        increThreeDvar.forecastState = increThreeDvar.getForecastState(increThreeDvar.analysisState, nowT=nowT)
+        increThreeDvar.analysisState = increThreeDvar.getAnalysisState(backgroundState = increThreeDvar.forecastState, \
+                                                                       observationState = increThreeDvar.observationState, \
+                                                                       backgroundEC = increThreeDvar.forecastEC, \
+                                                                       observationEC = increThreeDvar.observationEC)
 
-
-
+        increThreeDvar.RMSE = np.sqrt(np.mean((increThreeDvar.analysisState - xTruth[tidx+1])**2))
+        increThreeDvar.MeanError = np.mean(increThreeDvar.analysisState - xTruth[tidx+1])
+        dataRecorder.record(increThreeDvar, tidx=tidx+1)
+        print("{:02f}: {:05f}".format(nowT+dT, increThreeDvar.RMSE))
+    dataRecorder.saveToTxt()
